79488fe57def33af86da9bb1f3e5675fb9d46446
Eddie <eocampo52@gmail.com>
2025-04-16 22:35:09 -0500

    working on the main carousel section of movie rating project

---
+++ src/components/Main.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Main.css'
+
+function Main() {
+  return (
+    <div className='max-w-screen-xl mx-auto'>
+      <section className='grid grid-cols-3'>
+        <div className="col-span-2">
+          <div className="relative ">
+          {/* Image Div */}
+          <div className="banner-slider bg-cover">
+            <div className=""></div>
+          </div>
+          {/* Positioned abosluite Div */}
+          <div className="">
+            <p></p>
+          </div>
+          </div>
+        </div>
+        <div className=""></div>
+      </section>
+    </div>
+  )
+}
+
+export default Main
